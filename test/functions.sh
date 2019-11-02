8af021cd0b3432640800fa7c1c868f58b158f1ae
jiro4989 <jiroron666@gmail.com>
2019-11-02 12:20:04 +0900

    :white_check_mark: runの後にcoverageを取得するように処理を追加 #63
    :white_check_mark: batsのrunをカバレッジを取得するための関数に変更 #63
    
    :white_check_mark: runの後にcoverageを取得するように処理を追加 #63

--- test/functions.sh
+++ test/functions.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-run_test() {
-  run "$@"
-  kcov --bash-dont-parse-binary-dir ../coverage "$@"
+coverage() {
+  # kcovのインストールされている環境でのみ実行
+  if type kcov 2> /dev/null; then
+    kcov --bash-dont-parse-binary-dir coverage "$@" || true
+  fi
 }
