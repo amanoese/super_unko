7eceabfa934ee876e9c7eb9777151f90194b7ac5
jiro4989 <jiroron666@gmail.com>
2019-11-08 20:05:05 +0900

    :green_heart: TravisCI上で直接カバレッジを送信するように変更 #63
    :bug: カバレッジのディレクトリを生成するように変更 #63
    
    wip
    
    :wip: bats内ではやらないことにした
    
    debug
    
    wip ソースコードのマウント先を修正
    
    directory
    
    fix
    
    chmod
    
    wip
    fix
    
    fix

--- test/functions.sh
+++ test/functions.sh
@@ -4,12 +4,6 @@ coverage() {
   # kcovのインストールされている環境でのみ実行
   if type kcov 2> /dev/null; then
     local options=(--bash-dont-parse-binary-dir)
-
-    # CI上で実行されているときだけオプションを追加
-    if [[ ! "$TRAVIS_JOB_ID" == "" ]]; then
-      options+=("--coveralls-id=$TRAVIS_JOB_ID")
-    fi
-
     kcov "${options[@]}" coverage "$@" || true
   fi
 }
